2dcfdf31d212a5794e942298c05abf9c3cbe39aa
Angels101 <angelscodex101@gmail.com>
2020-07-31 12:10:19 +0300

    Recovered workload

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,34 @@ $(function () {
   });
 
 
+
+
   $( "li" ).add( "p" ).css( "background-color", "red" );
   $( "li" ).add( document.getElementsByTagName( "p" )[ 0 ] )
   .css( "background-color", "red" );
 
-  
+  ('.inittiallyHiddenMain2').hover(function () {
+    $('.initiallyHidden2').toggle();
+});
+$('.inittiallyHiddenMain3').hover(function () {
+    $('.initiallyHidden3').toggle();
+});
+$('.inittiallyHiddenMain4').hover(function () {
+    $('.initiallyHidden4').toggle();
+});
+$('.inittiallyHiddenMain5').hover(function () {
+    $('.initiallyHidden5').toggle();
+});
+$('.inittiallyHiddenMain6').hover(function () {
+    $('.initiallyHidden6').toggle();
+});
+$('.inittiallyHiddenMain7').hover(function () {
+    $('.initiallyHidden7').toggle();
+});
+$('.inittiallyHiddenMain8').hover(function () {
+    $('.initiallyHidden8').toggle();
+});
+event.preventDefault();
+});
+
+
