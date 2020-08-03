64a95de21c3b2f8c652fe582faf82e01ed0cec05
Angels101 <angelscodex101@gmail.com>
2020-08-03 14:11:19 +0300

    Adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
+prompt("SOS! Type your name and Click Ok!");
 $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar-fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
-/*----Three Column section-----*/ 
+});
 $(document).ready(function() {
-    $(".toggle").click(function() {
-      $("#shown-design").toggle();
-      $("#hidden-design").toggle();
-    });
+  $(".toggle").click(function() {
+    $("#shown-design").toggle();
+    $("#hidden-design").toggle();
   });
-  $(document).ready(function() {
-    $(".toggle1").click(function() {
-      $("#shown-development").toggle();
-      $("#hidden-development").toggle();
-    });
+});
+$(document).ready(function() {
+  $(".toggle1").click(function() {
+    $("#shown-development").toggle();
+    $("#hidden-development").toggle();
   });
-  $(document).ready(function() {
-    $(".toggle2").click(function() {
-      $("#shown-product").toggle();
-      $("#hidden-product").toggle();
-    });
+});
+$(document).ready(function() {
+  $(".toggle2").click(function() {
+    $("#shown-product").toggle();
+    $("#hidden-product").toggle();
   });
-   
-
-/*Portfolio*/
+});
+ 
 $(document).ready(function() {
   $(".cruiser").mouseover(function() {
     $("#g1").show()
@@ -76,200 +74,9 @@ $(document).ready(function() {
     $("#g8").hide()
   })
 })
-
 $(document).ready(function(){
-    $(".section6").submit(function(){
-      alert("WE have received your message ,thank you for reaching out to Delani studio");
-    });
+  $(".section6").submit(function(){
+    alert("WE have received your message ,thank you for reaching out to Delani studio");
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-
-
-    $(".content-box").click(function(){
-        $(".content-box").animate({
-            width:'+=500px',
-           height: '+=300px'
-        }, 1000);
-        $(".content-after").show().animate({opacity: '1'}, 3000);
-    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-!function(c,h,i,m,p){
-    m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.
-insertBefore(m,p)
-}(document,"script",
-"https://chimpstatic.com/mcjs-connected/js/users/edebb03d7696eb4e1486b8c63/4b086b83bf3b2d5332608c3ab.js");
-
-
-
-
-
-
-
-
-
-
-
-
-
-('.inittiallyHiddenMain2').hover(function () {
-    $('.initiallyHidden2').toggle();
-});
-$('.inittiallyHiddenMain3').hover(function () {
-    $('.initiallyHidden3').toggle();
-});
-$('.inittiallyHiddenMain4').hover(function () {
-    $('.initiallyHidden4').toggle();
-});
-$('.inittiallyHiddenMain5').hover(function () {
-    $('.initiallyHidden5').toggle();
-});
-$('.inittiallyHiddenMain6').hover(function () {
-    $('.initiallyHidden6').toggle();
-});
-$('.inittiallyHiddenMain7').hover(function () {
-    $('.initiallyHidden7').toggle();
-});
-$('.inittiallyHiddenMain8').hover(function () {
-    $('.initiallyHidden8').toggle();
-});
-event.preventDefault();
-});
-
-/*fgrz
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-  $( "li" ).add( "p" ).css( "background-color", "red" );
-  $( "li" ).add( document.getElementsByTagName( "p" )[ 0 ] )
-  .css( "background-color", "red" );
-
-  ('.inittiallyHiddenMain2').hover(function () {
-    $('.initiallyHidden2').toggle();
-});
-$('.inittiallyHiddenMain3').hover(function () {
-    $('.initiallyHidden3').toggle();
-});
-$('.inittiallyHiddenMain4').hover(function () {
-    $('.initiallyHidden4').toggle();
-});
-$('.inittiallyHiddenMain5').hover(function () {
-    $('.initiallyHidden5').toggle();
-});
-$('.inittiallyHiddenMain6').hover(function () {
-    $('.initiallyHidden6').toggle();
-});
-$('.inittiallyHiddenMain7').hover(function () {
-    $('.initiallyHidden7').toggle();
-});
-$('.inittiallyHiddenMain8').hover(function () {
-    $('.initiallyHidden8').toggle();
-});
-event.preventDefault();
-});
-
 
